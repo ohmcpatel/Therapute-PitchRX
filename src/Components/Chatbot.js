43e236c5a7f5dae57c7189265feed5cafc3d0bd6
Ohm Patel <ohmpatel1212@gmail.com>
2024-03-14 18:02:43 -0400

    new

--- src/Components/Chatbot.js
+++ src/Components/Chatbot.js
@@ -6,13 +6,15 @@ const Chatbot = () => {
   const [messages, setMessages] = useState([]);
   const [userEntry, setUserEntry] = useState('');
 
+
+
   const handleMessageSend = (messageText, sender) => {
     setMessages((prevMessages) => [...prevMessages, { text: messageText, sender }]);
   };
 
   const sendPromptToServer = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/generate-response', { prompt: "The following is how a user feels doing the dumbell thrust exercise. Ask them questions to figure out more about how their exercise went. Be specific and anaytical. But also be super short and concise. 1 sentence." + userEntry  + "now here is a log of all previous messages you sent so you can provide best response" + messages});
+      const response = await axios.post('http://localhost:3001/generate-response', { prompt: "The following is how a user feels doing the dumbell thrust exercise. Ask them questions to figure out more about how their exercise went. Be specific and anaytical. But also be super short and concise. 1 sentence. Here is what the user has to say" + userEntry  + "now here is a log of all previous messages you sent so you can provide best response" + messages});
 
       console.log('Server response:', response.data.response);
 
@@ -23,7 +25,13 @@ const Chatbot = () => {
   };
 
   useEffect(() => {
-    sendPromptToServer();
+    handleMessageSend("Hey, so you did the dumbell thrust exercise right? How did it feel overall?", 'bot'); // Assuming the response contains the message from the bot
+  }, [])
+
+  useEffect(() => {
+    if (userEntry.trim() !== '') { // Check if userEntry is not empty
+      sendPromptToServer();
+    }
   }, [userEntry]);
 
   return (
